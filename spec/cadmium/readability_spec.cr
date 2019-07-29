0c197112a5f37c29a5a3deaf2dd024b46f063d4f
Rémy Marronnier <rmarronnier@gmail.com>
2019-07-30 01:21:31 +0200

    Adds readability specs

--- spec/cadmium/readability_spec.cr
+++ spec/cadmium/readability_spec.cr
@@ -1,5 +1,231 @@
 require "../spec_helper"
 
 describe Cadmium::Readability do
+  # Long text of more than 30 sentences necessary to trigger SMOG calculation.
 
+  long_text = "Human reason, in one sphere of its cognition, is called upon to
+    consider questions, which it cannot decline, as they are presented by
+    its own nature, but which it cannot answer, as they transcend every
+    faculty of the mind.
+
+    It falls into this difficulty without any fault of its own. It begins
+    with principles, which cannot be dispensed with in the field of
+    experience, and the truth and sufficiency of which are, at the same
+    time, insured by experience. With these principles it rises, in
+    obedience to the laws of its own nature, to ever higher and more remote
+    conditions. But it quickly discovers that, in this way, its labours
+    must remain ever incomplete, because new questions never cease to
+    present themselves; and thus it finds itself compelled to have recourse
+    to principles which transcend the region of experience, while they are
+    regarded by common sense without distrust. It thus falls into confusion
+    and contradictions, from which it conjectures the presence of latent
+    errors, which, however, it is unable to discover, because the
+    principles it employs, transcending the limits of experience, cannot be
+    tested by that criterion. The arena of these endless contests is called
+    Metaphysic.
+
+    Time was, when she was the queen of all the sciences; and, if we take
+    the will for the deed, she certainly deserves, so far as regards the
+    high importance of her object-matter, this title of honour. Now, it is
+    the fashion of the time to heap contempt and scorn upon her; and the
+    matron mourns, forlorn and forsaken, like Hecuba:
+
+    Modo maxima rerum,
+    Tot generis, natisque potens...
+    Nunc trahor exul, inops.
+    —Ovid, Metamorphoses. xiii
+
+    At first, her government, under the administration of the dogmatists,
+    was an absolute despotism. But, as the legislative continued to show
+    traces of the ancient barbaric rule, her empire gradually broke up, and
+    intestine wars introduced the reign of anarchy; while the sceptics,
+    like nomadic tribes, who hate a permanent habitation and settled mode
+    of living, attacked from time to time those who had organized
+    themselves into civil communities. But their number was, very happily,
+    small; and thus they could not entirely put a stop to the exertions of
+    those who persisted in raising new edifices, although on no settled or
+    uniform plan. In recent times the hope dawned upon us of seeing those
+    disputes settled, and the legitimacy of her claims established by a
+    kind of physiology of the human understanding—that of the celebrated
+    Locke. But it was found that—although it was affirmed that this
+    so-called queen could not refer her descent to any higher source than
+    that of common experience, a circumstance which necessarily brought
+    suspicion on her claims—as this genealogy was incorrect, she persisted
+    in the advancement of her claims to sovereignty. Thus metaphysics
+    necessarily fell back into the antiquated and rotten constitution of
+    dogmatism, and again became obnoxious to the contempt from which
+    efforts had been made to save it. At present, as all methods, according
+    to the general persuasion, have been tried in vain, there reigns nought
+    but weariness and complete indifferentism—the mother of chaos and night
+    in the scientific world, but at the same time the source of, or at
+    least the prelude to, the re-creation and reinstallation of a science,
+    when it has fallen into confusion, obscurity, and disuse from ill
+    directed effort.
+
+    For it is in reality vain to profess indifference in regard to such
+    inquiries, the object of which cannot be indifferent to humanity.
+    Besides, these pretended indifferentists, however much they may try to
+    disguise themselves by the assumption of a popular style and by changes
+    on the language of the schools, unavoidably fall into metaphysical
+    declarations and propositions, which they profess to regard with so
+    much contempt. At the same time, this indifference, which has arisen in
+    the world of science, and which relates to that kind of knowledge which
+    we should wish to see destroyed the last, is a phenomenon that well
+    deserves our attention and reflection. It is plainly not the effect of
+    the levity, but of the matured judgement* of the age, which refuses to
+    be any longer entertained with illusory knowledge, It is, in fact, a
+    call to reason, again to undertake the most laborious of all tasks—that
+    of self-examination, and to establish a tribunal, which may secure it
+    in its well-grounded claims, while it pronounces against all baseless
+    assumptions and pretensions, not in an arbitrary manner, but according
+    to its own eternal and unchangeable laws. This tribunal is nothing less
+    than the critical investigation of pure reason.
+
+    I do not mean by this a criticism of books and systems, but a critical
+    inquiry into the faculty of reason, with reference to the cognitions to
+    which it strives to attain without the aid of experience; in other
+    words, the solution of the question regarding the possibility or
+    impossibility of metaphysics, and the determination of the origin, as
+    well as of the extent and limits of this science. All this must be done
+    on the basis of principles.
+
+    This path—the only one now remaining—has been entered upon by me; and I
+    flatter myself that I have, in this way, discovered the cause of—and
+    consequently the mode of removing—all the errors which have hitherto
+    set reason at variance with itself, in the sphere of non-empirical
+    thought. I have not returned an evasive answer to the questions of
+    reason, by alleging the inability and limitation of the faculties of
+    the mind; I have, on the contrary, examined them completely in the
+    light of principles, and, after having discovered the cause of the
+    doubts and contradictions into which reason fell, have solved them to
+    its perfect satisfaction. It is true, these questions have not been
+    solved as dogmatism, in its vain fancies and desires, had expected; for
+    it can only be satisfied by the exercise of magical arts, and of these
+    I have no knowledge. But neither do these come within the compass of
+    our mental powers; and it was the duty of philosophy to destroy the
+    illusions which had their origin in misconceptions, whatever darling
+    hopes and valued expectations may be ruined by its explanations. My
+    chief aim in this work has been thoroughness; and I make bold to say
+    that there is not a single metaphysical problem that does not find its
+    solution, or at least the key to its solution, here. Pure reason is a
+    perfect unity; and therefore, if the principle presented by it prove to
+    be insufficient for the solution of even a single one of those
+    questions to which the very nature of reason gives birth, we must
+    reject it, as we could not be perfectly certain of its sufficiency in
+    the case of the others.
+    "
+  short_text = "My training is a 360 approach. I first maintain a healthy cellular routine where I maximize the function of my mitochondria with supplements such as NAD+, Acetyl L-Carnitine, Magnesium, etc. This helps promote ATP and it’s incredibly visceral. From that point I spend 2-4 hours in my deprivation tank, this allows me to “astro-glide” to other dimensions - past, present, and future.
+    In the afternoons I do a 1-2 hour sword fighting session with my trainer, James Lew, we go over the fundamentals that work the obliques, core stabilizes, and triceps as well as a few tricks. To wind down from this I spend 30-45 minutes on an inclined hike at roughly 4-4.5 miles per hour, arguably the most efficient workout.
+    I then spend 45 minutes stretching before heading into the studio where my mind and body are functioning at peak level, with a neuroplastic goal between 57.5 and 71.5 AphC’s (which is my preferred range for my blood type). I’ve outfitted my studio with the highest grade of red light. It is pretty much 1000 sqf IR Sauna.
+    Hana then comes over and we do a screaming session for 20-25 minutes while I slow boil the honey tea that maximizes vocal proficiency. I have also eliminated all blue light from my vision through an experimental surgery that removes the top film of my eyeball and replaces it with an orange ultra-flex polymer that my friend and I made in the lab this past winter as a means to cure seasonal depression.
+    I go to bed with a humidifier on."
+
+  random_text = "vs/5d s/*gf '/'g 5*/ b5/*"
+
+  long_subject = Cadmium::Readability.new(long_text)
+  short_subject = Cadmium::Readability.new(short_text)
+  random_subject = Cadmium::Readability.new(random_text)
+
+  results =
+    {
+      "long" => {
+        "Number of paragraphs"        => 103,
+        "Number of sentences"         => 32,
+        "Number of words"             => 1122,
+        "Number of characters"        => 6667,
+        "Average words per sentence"  => 35.06,
+        "Average syllables per word"  => 1.60,
+        "Flesch score"                => 35.60,
+        "Flesch-Kincaid grade level"  => 17.00,
+        "Fog Index"                   => 21.01,
+        "SMOG grade level"            => 17.24,
+        "Automated Readability Index" => 19.62,
+        "Coleman-Liau Index"          => 18.28,
+        "LIX Index"                   => 59.00,
+        "Linsear Write Index"         => 23.19,
+      },
+      "short" => {
+        "Number of paragraphs"        => 5,
+        "Number of sentences"         => 11,
+        "Number of words"             => 252,
+        "Number of characters"        => 1468,
+        "Average words per sentence"  => 22.91,
+        "Average syllables per word"  => 1.51,
+        "Flesch score"                => 56.01,
+        "Flesch-Kincaid grade level"  => 11.14,
+        "Fog Index"                   => 14.40,
+        "SMOG grade level"            => 0.00,
+        "Automated Readability Index" => 13.12,
+        "Coleman-Liau Index"          => 16.83,
+        "LIX Index"                   => 45.00,
+        "Linsear Write Index"         => 11.47,
+      },
+      "random" => {
+        "Number of paragraphs"        => 1,
+        "Number of sentences"         => 1,
+        "Number of words"             => 4,
+        "Number of characters"        => 25,
+        "Average words per sentence"  => 4.00,
+        "Average syllables per word"  => 1.00,
+        "Flesch score"                => 118.18,
+        "Flesch-Kincaid grade level"  => -2.23,
+        "Fog Index"                   => 1.60,
+        "SMOG grade level"            => 0.00,
+        "Automated Readability Index" => 8.83,
+        "Coleman-Liau Index"          => 0.00,
+        "LIX Index"                   => 4.00,
+        "Linsear Write Index"         => 0.00,
+      },
+    }
+
+  it "should calculate correct values for all metrics when all test requirements are met" do
+    long_subject.num_paragraphs.should eq(results["long"]["Number of paragraphs"])
+    long_subject.num_sentences.should eq(results["long"]["Number of sentences"])
+    long_subject.num_words.should eq(results["long"]["Number of words"])
+    long_subject.num_chars.should eq(results["long"]["Number of characters"])
+    long_subject.words_per_sentence.should eq(results["long"]["Average words per sentence"])
+    long_subject.syllables_per_word.should eq(results["long"]["Average syllables per word"])
+    long_subject.flesch.should eq(results["long"]["Flesch score"])
+    long_subject.kincaid.should eq(results["long"]["Flesch-Kincaid grade level"])
+    long_subject.fog.should eq(results["long"]["Fog Index"])
+    long_subject.smog.should eq(results["long"]["SMOG grade level"])
+    long_subject.ari.should eq(results["long"]["Automated Readability Index"])
+    long_subject.coleman_liau.should eq(results["long"]["Coleman-Liau Index"])
+    long_subject.lix.should eq(results["long"]["LIX Index"])
+    long_subject.linsear_write.should eq(results["long"]["Linsear Write Index"])
+  end
+
+  it "should calculate correct values for applicable tests for a short text or return 0.0 when the test requirements are not met" do
+    short_subject.num_paragraphs.should eq(results["short"]["Number of paragraphs"])
+    short_subject.num_sentences.should eq(results["short"]["Number of sentences"])
+    short_subject.num_words.should eq(results["short"]["Number of words"])
+    short_subject.num_chars.should eq(results["short"]["Number of characters"])
+    short_subject.words_per_sentence.should eq(results["short"]["Average words per sentence"])
+    short_subject.syllables_per_word.should eq(results["short"]["Average syllables per word"])
+    short_subject.flesch.should eq(results["short"]["Flesch score"])
+    short_subject.kincaid.should eq(results["short"]["Flesch-Kincaid grade level"])
+    short_subject.fog.should eq(results["short"]["Fog Index"])
+    short_subject.smog.should eq(results["short"]["SMOG grade level"])
+    short_subject.ari.should eq(results["short"]["Automated Readability Index"])
+    short_subject.coleman_liau.should eq(results["short"]["Coleman-Liau Index"])
+    short_subject.lix.should eq(results["short"]["LIX Index"])
+    short_subject.linsear_write.should eq(results["short"]["Linsear Write Index"])
+  end
+
+  it "should return 0.0 instead of infinite or NaN values when analysing random characters" do
+    random_subject.num_paragraphs.should eq(results["random"]["Number of paragraphs"])
+    random_subject.num_sentences.should eq(results["random"]["Number of sentences"])
+    random_subject.num_words.should eq(results["random"]["Number of words"])
+    random_subject.num_chars.should eq(results["random"]["Number of characters"])
+    random_subject.words_per_sentence.should eq(results["random"]["Average words per sentence"])
+    random_subject.syllables_per_word.should eq(results["random"]["Average syllables per word"])
+    random_subject.flesch.should eq(results["random"]["Flesch score"])
+    random_subject.kincaid.should eq(results["random"]["Flesch-Kincaid grade level"])
+    random_subject.fog.should eq(results["random"]["Fog Index"])
+    random_subject.smog.should eq(results["random"]["SMOG grade level"])
+    random_subject.ari.should eq(results["random"]["Automated Readability Index"])
+    random_subject.coleman_liau.should eq(results["random"]["Coleman-Liau Index"])
+    random_subject.lix.should eq(results["random"]["LIX Index"])
+    random_subject.linsear_write.should eq(results["random"]["Linsear Write Index"])
+  end
 end
