d66d4624c411e8432663b58c5d3b9b6026b78a98
Rémy Marronnier <rmarronnier@gmail.com>
2019-08-01 23:52:01 +0200

    Abstraction finished

--- src/cadmium/summarizer/luhn_summarizer.cr
+++ src/cadmium/summarizer/luhn_summarizer.cr
@@ -2,42 +2,34 @@ require "./summarizer"
 
 module Cadmium
   # The Luhn summarizer returns the most significant sentences of a text by :
-  # 1 - Calculating occurences ratio of significant words (discounting ones with ratio outside an arbitray range, ie "normalized").
-  # 2 - Calculating each sentence weight ((number of significant words)² / (greatest distance between two significant words)).
+  # 1 - Calculating frequencies ratio of significant terms (discounting ones with ratio outside an arbitray range, ie "normalized").
+  # 2 - Calculating each sentence rating ((number of significant terms)² / (greatest distance between two significant terms)).
   # 3 - Sorting sentences according to their weight and returning the first n of them.
   # Reference : https://ieeexplore.ieee.org/document/5392672?arnumber=5392672
   class LuhnSummarizer < Summarizer
-    private def window_start(words_in_sentence : Array(String), keywords : Array(String)) : Int32 | Nil
-      words_in_sentence.index { |word| keywords.includes?(word) }
+    private def window_start(terms_in_sentence : Array(String), normalized_terms : Array(String)) : Int32 | Nil
+      terms_in_sentence.index { |term| normalized_terms.includes?(term) }
     end
 
-    private def window_end(words_in_sentence : Array(String), keywords : Array(String)) : Int32 | Nil
-      words_in_sentence[-1..0].index { |word| keywords.includes?(word) }
+    private def window_end(terms_in_sentence : Array(String), normalized_terms : Array(String)) : Int32 | Nil
+      terms_in_sentence[-1..0].index { |term| normalized_terms.includes?(term) }
     end
 
-    private def window_size(words_in_sentence : Array(String), keywords : Array(String)) : Int32
-      return 0 unless (window_start = window_start(words_in_sentence, keywords))
-      return 0 unless (window_end = window_end(words_in_sentence, keywords))
+    private def window_size(terms_in_sentence : Array(String), normalized_terms : Array(String)) : Int32
+      return 0 unless (window_start = window_start(terms_in_sentence, normalized_terms))
+      return 0 unless (window_end = window_end(terms_in_sentence, normalized_terms))
       return 0 if window_start.not_nil! > window_end.not_nil!
       window_end.not_nil! - window_start.not_nil! + 1
     end
 
-    private def sentence_weight(sentence : String, normalized_terms : Array(String)) : Int32
+    private def sentence_rating(sentence : String, normalized_terms_ratio : Hash(String, Float64)) : Int32
       terms_in_sentence = all_terms(sentence)
       return 0 if terms_in_sentence.size <= 1
+      normalized_terms = normalized_terms_ratio.keys
       window_size = window_size(terms_in_sentence, normalized_terms)
       return 0 if window_size <= 0
-      number_of_normalized_terms = terms_in_sentence.count { |word| normalized_terms.includes?(word) }
+      number_of_normalized_terms = terms_in_sentence.count { |term| normalized_terms.includes?(term) }
       (number_of_normalized_terms*number_of_normalized_terms) / window_size
-    end
-
-    def summarize(text : String, max_num_sentences = 5) : String
-      return "" if max_num_sentences <= 0
-      normalized_terms = normalized_terms(text)
-      return "" if normalized_terms.nil?
-      sentences = Cadmium::Util::Sentence.sentences(text)
-      sentences.sort_by! { |sentence| -sentence_weight(sentence, normalized_terms) }
-      sentences[0..max_num_sentences].join(" ")
     end
   end
 end
--- src/cadmium/summarizer/summarizer.cr
+++ src/cadmium/summarizer/summarizer.cr
@@ -1,6 +1,7 @@
 module Cadmium
   abstract class Summarizer
-    abstract def summarize(text : String, max_num_sentences = 5) : String
+    # All extraction-based summarizer must implement the sentence_rating method.
+    abstract def sentence_rating(sentence : String, normalized_terms_ratio : Hash(String, Float64)) : Int32
 
     def all_terms(text : String) : Array(String)
       text.tokenize(WordTokenizer)
@@ -23,11 +24,6 @@ module Cadmium
       normalize_ratio(terms_ratio(terms_frequencies(significant_terms), number_of_terms), min_ratio, max_ratio) # This is ugly, should find a better way to chain.
     end
 
-    def normalized_terms(text : String, min_ratio = 0.001, max_ratio = 0.5) : Array(String) | Nil
-      return nil unless normalized_terms = normalized_terms_ratio(text, min_ratio, max_ratio)
-      normalized_terms.keys
-    end
-
     def terms_frequencies(terms : Array(String)) : Hash(String, Int32) # Should we use Cadmium Tf instead ? Maybe factor it out in Cadmium::Util ?
       terms_frequencies = {} of String => Int32
 
@@ -48,6 +44,16 @@ module Cadmium
 
     def normalize_ratio(terms_ratio : Hash(String, Float64), min_ratio = 0.001, max_ratio = 0.5) : Hash(String, Float64) # Should we make min_ratio and max_ratio configurable by the user ?
       terms_ratio.delete_if { |_, ratio| !(min_ratio..max_ratio).includes?(ratio) }
+    end
+
+    # This summarize method should be common to all extraction-based summarizers as they all build a summary out of extracted and rated sentences
+    def summarize(text : String, max_num_sentences = 5) : String
+      return "" if max_num_sentences <= 0
+      normalized_terms_ratio = normalized_terms_ratio(text)
+      return "" if normalized_terms_ratio.nil?
+      sentences = Cadmium::Util::Sentence.sentences(text)
+      sentences.sort_by! { |sentence| -sentence_rating(sentence, normalized_terms_ratio) }
+      sentences[0..max_num_sentences].join(" ")
     end
   end
 end
