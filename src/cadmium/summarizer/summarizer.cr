5b0eb6eaa2b691019c7e9bb5b0d3724be0119e10
Rémy Marronnier <rmarronnier@gmail.com>
2019-08-01 04:30:58 +0200

    Uses WordTokenizer for performance improvements

--- src/cadmium/summarizer/summarizer.cr
+++ src/cadmium/summarizer/summarizer.cr
@@ -2,24 +2,8 @@ module Cadmium
   abstract class Summarizer
     abstract def summarize(text : String, max_num_sentences = 5) : String
 
-    # The following algo is faster than the pragmatic tokenizer but is the loss of quality output put worth it ?
-    # Taken from the Readability class. Should we replace it there by a proper tokenizer ?
-    # def all_words(text)
-    #   all_words = [] of String
-    #   text.scan(/\b([a-z][a-z\-']*)\b/i).each do |match|
-    #     word = match[0]
-    #     all_words << word
-    #   end
-    #   all_words
-    # end
-
     def all_words(text : String) : Array(String)
-      Cadmium::PragmaticTokenizer.new(
-        clean: true,
-        remove_stop_words: false,
-        punctuation: :none,
-        downcase: true
-      ).tokenize(text)
+      Cadmium::WordTokenizer.new.tokenize(text)
     end
 
     def significant_words(text : String) : Array(String) # Should we add a stemmer ?
