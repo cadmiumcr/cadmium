d8d957381fa356e91f7b39a724f0d3f9ef3b90f8
Rémy Marronnier <rmarronnier@gmail.com>
2019-08-22 01:11:18 +0200

    Variadic + all_languages flag added to the macro

--- src/cadmium/i18n/stop_words.cr
+++ src/cadmium/i18n/stop_words.cr
@@ -8,10 +8,18 @@ module Cadmium
       # from analysis.
       @@stop_words : Array(String) = {{ read_file("#{DATA_DIR.id}en.txt").split("\n") }}
 
-      macro stop_words(language)
+      macro stop_words(*languages)
+        {% if languages.includes?("all_languages") %}
+          files = Dir.open(DATA_DIR)
+          files.each_child do |file|
+            languages << file.chomp(".txt")
+          end
+        {% end %}
+          {% for language, index in languages %}
         def self.stop_words_{{language}} : Array(String)
         {{ read_file("#{DATA_DIR.id}#{language}.txt").split("\n") }}
         end
+        {% end %}
       end
     end
   end
